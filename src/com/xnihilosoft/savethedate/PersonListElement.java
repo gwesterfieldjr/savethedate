339226eb15755754ecf1adadaaca6f53aacc01c1
gwesterfieldjr <gwesterfieldjr@gmail.com>
2014-01-26 12:11:41 -0500

    created personlistelement, listadapter, etc. for couple to get married...

---
+++ src/com/xnihilosoft/savethedate/PersonListElement.java
@@ -0,0 +1,70 @@
+package com.xnihilosoft.savethedate;
+
+import android.view.View;
+import android.widget.BaseAdapter;
+
+public abstract class PersonListElement {
+
+	private String profilePictureId;
+	private String type;
+	private String name;
+	private int requestCode;
+	
+	private BaseAdapter adapter;
+	
+	public PersonListElement(int requestCode) {
+		super();
+		this.requestCode = requestCode;
+	}
+	
+	public void setAdapter(BaseAdapter adapter) {
+	    this.adapter = adapter;
+	}
+	
+	public String getProfilePictureId() {
+		return profilePictureId;
+	}
+	
+	public void setProfilePictureId(String profilePictureId) {
+		this.profilePictureId = profilePictureId;
+		
+		if (adapter != null) {
+		    adapter.notifyDataSetChanged();
+		}
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+		
+		if (adapter != null) {
+		    adapter.notifyDataSetChanged();
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+		
+		if (adapter != null) {
+		    adapter.notifyDataSetChanged();
+		}
+	}
+	
+	public int getRequestCode() {
+		return requestCode;
+	}
+	
+	public void setRequestCode(int requestCode) {
+		this.requestCode = requestCode;
+	}
+	
+	protected abstract View.OnClickListener getOnClickListener();
+	
+}
--- src/com/xnihilosoft/savethedate/SelectionFragment.java
+++ src/com/xnihilosoft/savethedate/SelectionFragment.java
@@ -1,12 +1,19 @@
 package com.xnihilosoft.savethedate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.Request;
@@ -21,11 +28,13 @@ public class SelectionFragment extends Fragment {
 
 	private static final String TAG = "SelectionFragment";
 	private static final int REAUTH_ACTIVITY_CODE = 100;
+	private static final int USER = 0;
+	private static final int SPOUSE_TO_BE = 1;
 	
-	private ProfilePictureView profilePictureView;
-	private TextView userNameView;
-	private TextView userTypeView;
 	private EditText noticeMessageView;
+	
+	private ListView listView;
+	private List<PersonListElement> personListElements;
 	
 	private UiLifecycleHelper uiHelper;
 	private Session.StatusCallback callback = new Session.StatusCallback() {
@@ -40,15 +49,32 @@ public class SelectionFragment extends Fragment {
 	    super.onCreateView(inflater, container, savedInstanceState);
 	    View view = inflater.inflate(R.layout.selection, container, false);
 	    
-		// Find the user's profile picture custom view
-	    profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
-	    profilePictureView.setCropped(true);
-	
-	    // Find the user's name view
-	    userNameView = (TextView) view.findViewById(R.id.selection_user_name);
+	    // Find the list view
+	    listView = (ListView) view.findViewById(R.id.selection_person_list);
 	    
-	    // Find the user's type view
-	    userTypeView = (TextView) view.findViewById(R.id.selection_user_type);
+	    // Set up the list view items, based on a list of
+	    // CoupleListElement items
+	    personListElements = new ArrayList<PersonListElement>();
+	    
+	    // Add an item for the current user
+	    personListElements.add(new PersonListElement(USER) {
+			@Override
+			protected OnClickListener getOnClickListener() {
+				return null;
+			}
+		});
+	    
+	    // Add item for the spouse to be friend picker
+	    personListElements.add(new PersonListElement(SPOUSE_TO_BE) {
+			@Override
+			protected OnClickListener getOnClickListener() {
+				
+				return null;
+			}
+		});
+	    
+	    // Set the list view adapter
+	    listView.setAdapter(new ActionListAdapter(getActivity(), R.id.selection_person_list, personListElements));
 	    
 	    // Find the notice message edit text area
 	    noticeMessageView = (EditText) view.findViewById(R.id.selection_notice_message);
@@ -74,16 +100,18 @@ public class SelectionFragment extends Fragment {
 	                if (user != null) {
 	                    // Set the id for the ProfilePictureView
 	                    // view that in turn displays the profile picture.
-	                    profilePictureView.setProfileId(user.getId());
+	                	personListElements.get(USER).setProfilePictureId(user.getId());
 	                    // Set the Textview's text to the user's name.
-	                    userNameView.setText(user.getName());
-	                    
+	                	personListElements.get(USER).setName(user.getName());
+	                	
 	                    // Set the userType TextView text to bride or groom
 	                    String gender = user.asMap().get("gender").toString();
-	                    if (gender.contains("male")) {
-	                    	userTypeView.setText("Groom");
+	                    if (gender.contains("female")) {
+	                    	personListElements.get(USER).setType("Bride");
+	                    } else if (gender.contains("male")) {
+	                    	personListElements.get(USER).setType("Groom");
 	                    } else {
-	                    	userTypeView.setText("Bride");
+	                    	personListElements.get(USER).setType("Other");
 	                    }
 	                }
 	            }
@@ -139,6 +167,50 @@ public class SelectionFragment extends Fragment {
 	    if (requestCode == REAUTH_ACTIVITY_CODE) {
 	        uiHelper.onActivityResult(requestCode, resultCode, data);
 	    }
+	}
+	
+	private class ActionListAdapter extends ArrayAdapter<PersonListElement> {
+	    private List<PersonListElement> listElements;
+
+	    public ActionListAdapter(Context context, int resourceId, 
+	                             List<PersonListElement> listElements) {
+	        super(context, resourceId, listElements);
+	        this.listElements = listElements;
+	        // Set up as an observer for list item changes to
+	        // refresh the view.
+	        for (int i = 0; i < listElements.size(); i++) {
+	            listElements.get(i).setAdapter(this);
+	        }
+	    }
+
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        View view = convertView;
+	        if (view == null) {
+	            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	            view = inflater.inflate(R.layout.personlistitem, null);
+	        }
+
+	        PersonListElement listElement = listElements.get(position);
+	        if (listElement != null) {
+	            view.setOnClickListener(listElement.getOnClickListener());
+	            ProfilePictureView profilePicture = (ProfilePictureView) view.findViewById(R.id.person_profile_pic);
+	            profilePicture.setCropped(true);
+	            TextView userType = (TextView) view.findViewById(R.id.person_type);
+	            TextView userName = (TextView) view.findViewById(R.id.person_name);
+	            if (profilePicture != null) {
+	            	profilePicture.setProfileId(listElement.getProfilePictureId());
+	            }
+	            if (userType != null) {
+	            	userType.setText(listElement.getType());
+	            }
+	            if (userType != null) {
+	            	userName.setText(listElement.getName());
+	            }
+	        }
+	        return view;
+	    }
+
 	}
 	
 }
